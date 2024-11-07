6d497c1243cca008adc1ad5e17da15b364a06c4c
Krzysztof Tomasz Zembrowski <zembrowski@users.noreply.github.com>
2024-11-07 16:50:08 +0100

    feat: add additional valid examples

--- tests/Datasets/Valid.php
+++ tests/Datasets/Valid.php
@@ -2,4 +2,25 @@
 
 dataset('valid', [
     'DE123456788',
+    // Following have been shared on: https://forum.elster.de/herstellerforum/forum/eric/plausibilit%C3%A4ten-im-eric/155024-wirtschafts-identifikationsnummer?p=155064#post155064
+    'DE230447822',
+    'DE247134274',
+    'DE221000383',
+    'DE223968406',
+    'DE205024813',
+    'DE239808630',
+    'DE236889989',
+    'DE127353650',
+    'DE293758340',
+    'DE254725846',
+    'DE217741235',
+    'DE299082893',
+    'DE192834565',
+    'DE284164018',
+    'DE232637828',
+    'DE291301324',
+    'DE219711424',
+    'DE237998736',
+    'DE220178622',
+    'DE191592010',
 ]);
